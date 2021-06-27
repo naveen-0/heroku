0fdd3123f585e9439c4d1ed72bd7b8da90da6040
Thanks God <talk2nave2n@gmail.com>
2021-06-27 13:39:53 +0530

    mod

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const hbs = require("hbs");
 // const partials = require("partials");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // public static path
 const publicPath = path.join(__dirname, "../public");
